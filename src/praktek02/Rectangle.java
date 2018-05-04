c64e7f6cbb5b11f78e633c6fbdae22f06d1138a5
triwahyu13 <34591200+triwahyu13@users.noreply.github.com>
2018-05-04 21:05:05 +0800

    Constructor - sama dengan nama class

--- src/praktek02/Rectangle.java
+++ src/praktek02/Rectangle.java
@@ -2,6 +2,11 @@ package praktek02;
 public class Rectangle {
     double panjang;
     double lebar;
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
     
     void cetakInfo(){
         System.out.println("=====================");
